947f8e8d2c68b1bc141bb54747506d999b44d649
martin byrne <marb61a@gmail.com>
2016-12-29 14:16:55 +0000

    added initial App.jsx file

---
+++ imports/ui/App.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import AppBar from 'material-ui/AppBar';
+import { List } from 'material-ui/List';
+import Divider from 'material-ui/Divider';
+
+import TeamList from './Team-list';
+import TeamStats from './Team-stats';
+import Player from './Player';
+
+export default class App extends Component{
+    constructor(props){
+        super(props);
+        
+        // Set up state
+        this.state = {players : []};
+    }
+    
+    componentWillMount(){
+        this.setState({});    
+    }
+    
+    renderPlayers(){
+        
+    }
+    
+    render(){
+        return(
+            <MuiThemeProvider>
+                <div className="container">
+                
+                </div>
+            </MuiThemeProvider>    
+        );
+    }
+}
