579446722c61281743c10b3d5f1410cedc7143a0
martin byrne <marb61a@gmail.com>
2017-01-04 10:02:25 +0000

    updated new jsx

--- imports/ui/New.jsx
+++ imports/ui/New.jsx
@@ -9,6 +9,14 @@ export default class Example extends Component {
         Players.insert({
             name : this.refs.name.value,
             team : this.refs.team.value,
+            ballManipulation : this.refs.ballManipulation.value,
+            kickingAbilities : this.refs.kickingAbilities.value,
+            passingAbilities: this.refs.passingAbilities.value,
+            duelTackling: this.refs.duelTackling.value,
+            fieldCoverage: this.refs.fieldCoverage.value,
+            blockingAbilities: this.refs.blockingAbilities.value,
+            gameStrategy: this.refs.gameStrategy.value,
+            playmakingRisks: this.refs.playmakingRisks.value,
             notes : this.refs.notes.value,
             createdAt : new Date()
         });
@@ -23,6 +31,34 @@ export default class Example extends Component {
             <div className="row">
                 <form className="col s12">
                     <h3>Add a new player</h3>
+                    <div className="row">
+                        <div className="input-field col s6">
+                            <input placeholder="Name" ref="name" type="text" className="validate"/>
+                        </div>
+                        <div className="input-field col s6">
+                            <input placeholder="Team" ref="team" type="text" className="validate"/>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="input-field col s6">
+                            <h5>Ball Manipulation</h5>
+                            <select className="browser-default" ref="ballManipulation">
+                                <option value="0">0 - Hasn't demonstrated skills</option>
+                                <option value="1">1 - Needs improvement</option>
+                                <option value="2">2 - Skill acquired</option>
+                                <option value="3">3 - Great skills/could teach</option>
+                            </select>
+                        </div>
+                        <div className="input-field col s6">
+                            <h5>Kicking Abilities</h5>
+                            <select className="browser-default" ref="ballManipulation">
+                                <option value="0">0 - Hasn't demonstrated skills</option>
+                                <option value="1">1 - Needs improvement</option>
+                                <option value="2">2 - Skill acquired</option>
+                                <option value="3">3 - Great skills/could teach</option>    
+                            </select>
+                        </div>
+                    </div>
                 </form>
             </div>        
         );
